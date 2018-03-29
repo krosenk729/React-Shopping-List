7465172db910dab9c290fca2abbc910f37a64bfb
krosenk729 <krosenk729@gmail.com>
2018-03-29 12:05:24 -0400

    updated list items

---
+++ src/sample-items.js
@@ -0,0 +1,67 @@
+// This is just some sample data so you don't have to think of your own!
+const fishes = {
+  item1: {
+    name: "Celery",
+    image: "/images/svg_celery.svg",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+    price: 1724,
+    status: "available"
+  },
+
+  item2: {
+    name: "Hummus",
+    image: "/images/svg_hummus.svg",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+    price: 3200,
+    status: "available"
+  },
+
+  item3: {
+    name: "H2O",
+    image: "/images/svg_water.svg",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+    price: 1684,
+    status: "unavailable"
+  },
+
+  item4: {
+    name: "Yoga Pants",
+    image: "/images/svg_yogapants.svg",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+    price: 1129,
+    status: "available"
+  },
+
+  item5: {
+    name: "Coffee",
+    image: "/images/svg_coffee.svg",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+    price: 4234,
+    status: "available"
+  },
+
+  item6: {
+    name: "Bobby Pins & Hair Ties",
+    image: "/images/svg_bobbypin.svg",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+    price: 1453,
+    status: "available"
+  },
+
+  item7: {
+    name: "Dog Toys",
+    image: "/images/svg_dogtoy.svg",
+    desc:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor.",
+    price: 2543,
+    status: "available"
+  }
+};
+
+export default fishes;
