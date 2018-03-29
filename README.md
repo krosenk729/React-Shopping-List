5e25490d62bd6718084e439b5effe0bcb877f448
krosenk729 <krosenk729@gmail.com>
2018-03-29 09:47:04 -0400

    baseline

---
+++ README.md
@@ -0,0 +1,11 @@
+# Coffee of the Day
+
+```
+npm install
+npm start
+```
+
+Made Changes? Run this first
+```
+npm run build
+```
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker />, document.querySelector('#main'));
