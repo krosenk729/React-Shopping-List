c5e776d465719e335437ea3c035578977c126066
krosenk729 <krosenk729@gmail.com>
2018-03-30 15:02:42 -0400

    updated styles for list items

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,12 +3,12 @@ import React from 'react';
 class Item extends React.Component{
 
 	render(){
-		const {image, name, price, status} = this.props.details;
+		const {image, name, price, desc, status} = this.props.details;
 		const needIt = status === 'need'
 		return (
-			<li>
-				<img src="{image}" alt={name} />
-				<h3>{name}</h3>
+			<li class="item">
+				<img src={image} alt={name} />
+				<h3>{name} : {status}</h3>
 			</li>
 		)
 	}
