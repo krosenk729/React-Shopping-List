1475406d9b0a387b623332f9389e146828302d1c
krosenk729 <krosenk729@gmail.com>
2018-03-30 19:26:36 -0400

    shopping list updated for out of need

--- src/components/StoreList.js
+++ src/components/StoreList.js
@@ -2,9 +2,20 @@ import React from 'react';
 import { formatPrice } from '../helpers';
 
 class StoreList extends React.Component{
-	renderListItem = (item) => (
-		<li>{item}</li>
-		)
+	renderListItem = (i) => {
+		const item = this.props.items[i];
+		const qty = this.props.list[i];
+		const isNeeded = item.status === 'need';
+		if(!isNeeded){
+			return(
+				<li>{item ? item.name : 'Deleted item'} | shop for 0!<br />this isn't needed anymore</li>
+				)
+		} else {
+			return(
+				<li>{item.name} | shop for {qty}</li>
+				)
+		}
+	}
 
 	render(){
 		const itemIds = Object.keys(this.props.list);
