40c00f8a8e5897d02fd526fd9b0559db585d6f2a
krosenk729 <krosenk729@gmail.com>
2018-03-30 19:09:41 -0400

    adding to store list

--- src/components/StoreList.js
+++ src/components/StoreList.js
@@ -1,13 +1,29 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class StoreList extends React.Component{
 	renderListItem = (item) => (
-		
+		<div>Test</div>
 		)
 
 	render(){
+		const itemIds = Object.keys(this.props.list);
+		const total = itemIds.reduce((t, i)=>{
+			const item = this.props.items[i];
+			const qty = this.props.list[i];
+			if(item && item.status === 'need'){
+				return t + (qty * item.price);
+			} else {
+				return t;
+			}
+		}, 0);
 		return (
-			<div className="test">
+			<div className="store-list">
+			<h2>Shopping List</h2>
+			<ul>
+			{itemIds.map(i => <li>{i}</li>)}
+			</ul>
+			<p>{formatPrice(total)}</p>
 			</div>
 		)
 	}
