5db1f5113af179e70f837c841cff256892a12eb8
krosenk729 <krosenk729@gmail.com>
2018-03-31 07:35:44 -0400

    adding prop-types for type checking

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import PropTypes from 'prop-types'; 
 
 const Header = (props)=>(
 	<h1>
 		{props.storeName} Favorites
 	</h1>
 );
+
+Header.propTypes = {
+	storeName: PropTypes.string.isRequired
+};
 
 export default Header;
