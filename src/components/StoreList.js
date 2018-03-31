a941ae3ca9b887b06566016631236501977c0730
krosenk729 <krosenk729@gmail.com>
2018-03-31 07:56:38 -0400

    adding prop-types for type checking

--- src/components/StoreList.js
+++ src/components/StoreList.js
@@ -1,8 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types'; 
 import { formatPrice } from '../helpers';
 import { TransitionGroup, CSSTransition } from 'react-transition-group';
 
 class StoreList extends React.Component{
+	static propTypes = {
+		items: PropTypes.object,
+		list: PropTypes.object,
+		addToList: PropTypes.func,
+		decreaseList: PropTypes.func,
+		removeFromList: PropTypes.func
+	}
+
 	renderListItem = (i) => {
 		const item = this.props.items[i];
 		if(!item) return null; 
@@ -20,43 +29,43 @@ class StoreList extends React.Component{
 			return(
 				<CSSTransition {...transitionOptions}>
 				<li key={i} className="store-list-item">
-					<h4>{item ? item.name : 'Deleted item'}</h4>
-					<p>shop for 0! </p>
-					<div>
-					<button className="del" onClick={()=> this.props.removeFromList(i)}><span role="img" aria-label="change quantity">⛔</span></button>
-					</div>
+				<h4>{item ? item.name : 'Deleted item'}</h4>
+				<p>shop for 0! </p>
+				<div>
+				<button className="del" onClick={()=> this.props.removeFromList(i)}><span role="img" aria-label="change quantity">⛔</span></button>
+				</div>
 				</li>
 				</CSSTransition>
 				)
-		} else {
-			return(
+			} else {
+				return(
 				<CSSTransition {...transitionOptions}>
 				<li key={i} className="store-list-item">
-					<h4>{item.name}</h4>
-					<p>shop for {qty}</p>
-					<div>
-					<button className="up" onClick={()=> this.props.addToList(i)}><span role="img" aria-label="change quantity">🔺</span></button>
-					<button className="down" onClick={()=> this.props.decreaseList(i)}><span role="img" aria-label="change quantity">🔻</span></button>
-					<button className="del" onClick={()=> this.props.removeFromList(i)}><span role="img" aria-label="change quantity">⛔</span></button>
-					</div>
+				<h4>{item.name}</h4>
+				<p>shop for {qty}</p>
+				<div>
+				<button className="up" onClick={()=> this.props.addToList(i)}><span role="img" aria-label="change quantity">🔺</span></button>
+				<button className="down" onClick={()=> this.props.decreaseList(i)}><span role="img" aria-label="change quantity">🔻</span></button>
+				<button className="del" onClick={()=> this.props.removeFromList(i)}><span role="img" aria-label="change quantity">⛔</span></button>
+				</div>
 				</li>
 				</CSSTransition>
 				)
-		}
-	}
-
-	render(){
-		const itemIds = Object.keys(this.props.list);
-		const total = itemIds.reduce((t, i)=>{
-			const item = this.props.items[i];
-			const qty = this.props.list[i];
-			if(item && item.status === 'need'){
-				return t + (qty * item.price);
-			} else {
-				return t;
 			}
-		}, 0);
-		return (
+		}
+
+		render(){
+			const itemIds = Object.keys(this.props.list);
+			const total = itemIds.reduce((t, i)=>{
+				const item = this.props.items[i];
+				const qty = this.props.list[i];
+				if(item && item.status === 'need'){
+					return t + (qty * item.price);
+				} else {
+					return t;
+				}
+			}, 0);
+			return (
 			<div className="store-list">
 			<h2>Shopping List for {this.props.storeName}</h2>
 			<TransitionGroup component="ul">
@@ -64,8 +73,8 @@ class StoreList extends React.Component{
 			</TransitionGroup>
 			<p>{formatPrice(total)}</p>
 			</div>
-		)
+			)
+		}
 	}
-}
 
-export default StoreList;
+	export default StoreList;
